2a3bc5aeed36ad7ac36ae95782a092155e1f4301
零零豆 <29398217@qq.com>
2023-07-25 04:19:01 +0800

    每天60s读懂世界

---
+++ 每天60s读懂世界.py
@@ -0,0 +1,15 @@
+# @author Sten
+# 觉得不错麻烦点个star谢谢
+# 使用青龙自带的通知
+import requests
+import json
+import notify
+url = 'https://60s.viki.moe/?encoding=text'  # 来自知乎的一位大佬的接口
+resp = requests.get(url)
+news = resp.text.replace('\n', '\n      \n    ')
+
+info = f"""
+{news}   
+"""
+
+notify.send("每天60s读懂世界", info)
